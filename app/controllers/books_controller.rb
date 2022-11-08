304261b9c7ec2be5fa267058f89d73f7e572269f
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-08 07:11:53 +0000

    [Add]新規登録機能/投稿一覧

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end 
 
   def index
+    @books = Book.all.order(id: :ASC)
   end
 
   def show
@@ -10,4 +18,9 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get '/top' => 'homes#top'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
+  get 'books/new' => 'books#new'
+  post 'books' => 'books#create'
+  get 'books'=> 'books#index'
+  
+  get 'books/show' => 'books#show'
+  get 'books/edit' => 'books#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
