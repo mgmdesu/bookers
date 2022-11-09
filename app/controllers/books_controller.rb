02b1d7264af20c2826d1c1f71f67156ce11d2d98
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-09 05:12:39 +0000

    [Add]バリデーションの実装(#create)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id)
+      redirect_to @book
     else 
-    render :new
+      @books = Book.all.order(id: :ASC)
+      render :index
     end 
   end 
 
