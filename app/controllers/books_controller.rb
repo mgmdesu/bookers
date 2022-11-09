ebf1a95b02506a80171fd8c2ffb807aee253146f
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-09 08:16:45 +0000

    [Add]機能の追加All OK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to @book
     else 
       @books = Book.all.order(id: :ASC)
@@ -24,15 +25,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end 
   end 
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end 
   
   private
